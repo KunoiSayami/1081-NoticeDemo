b8de43272bc098c105c44a9ef2d6e3d427a76e90
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2019-09-12 23:58:25 +0800

    Make Firebase workable

--- app/src/main/java/org/example/u/noticedemo/MainActivity.java
+++ app/src/main/java/org/example/u/noticedemo/MainActivity.java
@@ -1,5 +1,6 @@
 package org.example.u.noticedemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -8,16 +9,48 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
 
 	String CHANNEL_ID = "NoticeDemo";
+
+	String TAG = "MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		this.sendNotice();
+		this.init();
+	}
+
+	void init(){
+		FirebaseInstanceId.getInstance().getInstanceId()
+				.addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+					@Override
+					public void onComplete(@NonNull Task<InstanceIdResult> task) {
+						if (!task.isSuccessful()) {
+							Log.w(TAG, "getInstanceId failed", task.getException());
+							return;
+						}
+
+						// Get new Instance ID token
+						String token = task.getResult().getToken();
+
+						// Log and toast
+						String msg = getString(R.string.msg_token_fmt, token);
+						Log.d(TAG, msg);
+						Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+					}
+				});
+
 	}
 
 	void sendNotice(){
---
+++ app/src/main/java/org/example/u/noticedemo/MyFirebaseMessagingService.java
@@ -0,0 +1,68 @@
+package org.example.u.noticedemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+	String TAG = "MyFirebaseMessagingService";
+
+	@Override
+	public void onMessageReceived(RemoteMessage remoteMessage) {
+		// ...
+
+		// TODO(developer): Handle FCM messages here.
+		// Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+		Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+		// Check if message contains a data payload.
+		if (remoteMessage.getData().size() > 0) {
+			Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+		}
+
+		// Check if message contains a notification payload.
+		if (remoteMessage.getNotification() != null) {
+			Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+			this.sendNotice(remoteMessage.getNotification().getTitle(),
+					remoteMessage.getNotification().getBody());
+		}
+
+		// Also if you intend on generating your own notifications as a result of a received FCM
+		// message, here is where that should be initiated. See sendNotification method below.
+	}
+
+	@Override
+	public void onNewToken(String token) {
+		Log.d(TAG, "Refreshed token: " + token);
+
+		// If you want to send messages to this application instance or
+		// manage this apps subscriptions on the server side, send the
+		// Instance ID token to your app server.
+		sendRegistrationToServer(token);
+	}
+
+	private void sendRegistrationToServer(String refreshedToken) {
+		Log.d("TOKEN ", refreshedToken);
+	}
+
+
+	private void sendNotice(String title, String body) {
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "MESSAGECHANNEL")
+				.setSmallIcon(R.drawable.common_full_open_on_phone)
+				.setContentTitle(title)
+				.setContentText(body)
+				.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+		NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+		notificationManagerCompat.notify(1, builder.build());
+	}
+}

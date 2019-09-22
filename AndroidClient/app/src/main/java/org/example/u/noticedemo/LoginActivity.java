91870adf7a1d0d888b7e96d33211b0511c0f9cbb
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2019-09-23 00:39:02 +0800

    Merge register page and login page

--- AndroidClient/app/src/main/java/org/example/u/noticedemo/LoginActivity.java
+++ AndroidClient/app/src/main/java/org/example/u/noticedemo/LoginActivity.java
@@ -19,16 +19,20 @@
 */
 package org.example.u.noticedemo;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 class NetworkLoginException extends Exception {
 
@@ -36,10 +40,12 @@ class NetworkLoginException extends Exception {
 
 public class LoginActivity extends AppCompatActivity {
 
-	EditText etUser, etPassword;
+	EditText etUser, etPassword, etRepeatPassword;
 	Button btLogin, btGoRegister;
+	TextView txtTitle, txtRepeatPassword;
 	CheckBox cbRemember;
 
+	ArrayList<TextWatcher> arrayList;
 	String TAG = "log_LoginActivity";
 
 	@Override
@@ -49,20 +55,93 @@ public class LoginActivity extends AppCompatActivity {
 		this.init();
 	}
 
-	void init() {
-		etUser = findViewById(R.id.etUser);
-		etPassword = findViewById(R.id.etPassword);
-		btLogin = findViewById(R.id.btLogin);
-		cbRemember = findViewById(R.id.cbRemember);
-		btGoRegister = findViewById(R.id.btGoRegister);
+	void vaild_username() {
+		if (etUser.length() == 0)
+			btGoRegister.setEnabled(false);
+	}
 
-		cbRemember.setChecked(MainActivity.databaseHelper.isRememberedPassword());
+	void setOnTextChangeListener() {
 
-		if (cbRemember.isChecked()) {
-			String[] accountGroup = MainActivity.databaseHelper.getStoredUser();
-			etUser.setText(accountGroup[0]);
-			etPassword.setText(accountGroup[1]);
-		}
+		etUser.addTextChangedListener(arrayList.get(0));
+
+		// https://stackoverflow.com/a/20824665
+		etPassword.addTextChangedListener(arrayList.get(1));
+
+		etRepeatPassword.addTextChangedListener(arrayList.get(2));
+	}
+
+	void changeToRegisterLayout() {
+		txtTitle.setText(getString(R.string.text_title_register_page));
+		txtRepeatPassword.setVisibility(View.VISIBLE);
+		etRepeatPassword.setVisibility(View.VISIBLE);
+		cbRemember.setVisibility(View.INVISIBLE);
+		//btLogin.setVisibility(View.INVISIBLE);
+
+		btGoRegister.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				NetworkSupport networkSupport = new NetworkSupport();
+				String strRegUser, strRegPassword;
+				strRegUser = etUser.getText().toString();
+				strRegPassword = etUser.getText().toString();
+
+				if (strRegPassword.length() == 0) {
+					strRegUser = "test";
+					strRegPassword = "test";
+				}
+
+				try {
+					HttpRawResponse httpRawResponse =
+							networkSupport.doRegister(strRegUser, strRegPassword);
+					if (httpRawResponse.getStatus() == 200) {
+						//((EditText)findViewById(R.id.etUser)).setText(strRegUser);
+						//((EditText)findViewById(R.id.etPassword)).setText(strRegPassword);
+						Toast.makeText(LoginActivity.this, "Register success", Toast.LENGTH_SHORT).show();
+						// https://stackoverflow.com/a/4038637
+						backToLoginPage();
+					}
+					else {
+						throw new NetworkRegisterException();
+					}
+				} catch (Exception e){
+					e.printStackTrace();
+				}
+			}
+		});
+
+		btLogin.setText(R.string.text_back_button);
+		btLogin.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				backToLoginPage();
+			}
+		});
+		setOnTextChangeListener();
+	}
+
+	void backToLoginPage() {
+		txtTitle.setText(R.string.text_title_login_page);
+		txtRepeatPassword.setVisibility(View.INVISIBLE);
+		etRepeatPassword.setVisibility(View.INVISIBLE);
+		//btLogin.setVisibility(View.VISIBLE);
+		cbRemember.setVisibility(View.VISIBLE);
+
+		etPassword.removeTextChangedListener(arrayList.get(1));
+
+		btGoRegister.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				changeToRegisterLayout();
+			}
+		});
+
+		btLogin.setText(R.string.text_login);
+		initbtnlogin();
+
+
+	}
+
+	void initbtnlogin() {
 
 		btLogin.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -98,12 +177,90 @@ public class LoginActivity extends AppCompatActivity {
 			}
 		});
 
+	}
+
+	void init() {
+		etUser = findViewById(R.id.etUser);
+		etPassword = findViewById(R.id.etPassword);
+		btLogin = findViewById(R.id.btLogin);
+		cbRemember = findViewById(R.id.cbRemember);
+		btGoRegister = findViewById(R.id.btGoRegister);
+
+		txtTitle = findViewById(R.id.textLogin);
+		txtRepeatPassword = findViewById(R.id.txtRepeatPassword);
+		etRepeatPassword = findViewById(R.id.etRepeatPassword);
+
+		etRepeatPassword.setVisibility(View.INVISIBLE);
+		txtRepeatPassword.setVisibility(View.INVISIBLE);
+
+		cbRemember.setChecked(MainActivity.databaseHelper.isRememberedPassword());
+
+		if (cbRemember.isChecked()) {
+			String[] accountGroup = MainActivity.databaseHelper.getStoredUser();
+			etUser.setText(accountGroup[0]);
+			etPassword.setText(accountGroup[1]);
+		}
+
+		initbtnlogin();
+
 		btGoRegister.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-				startActivity(intent);
+				changeToRegisterLayout();
+				/*Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+				startActivity(intent);*/
 			}
 		});
+		appendTextWatcher();
+	}
+
+	void appendTextWatcher() {
+		arrayList.add(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				if (s.length() == 0)
+					btGoRegister.setEnabled(false);
+				else
+				if (etPassword.length() > 0 &&
+						etPassword.getText().toString().equals(etPassword.getText().toString()))
+					btGoRegister.setEnabled(true);
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) { }
+		});
+
+		arrayList.add(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// Should use toString() method
+				btGoRegister.setEnabled(etRepeatPassword.getText().toString().equals(s.toString()));
+				vaild_username();
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) { }
+		});
+
+		arrayList.add(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				btGoRegister.setEnabled(etPassword.getText().toString().equals(s.toString()));
+				vaild_username();
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) { }
+		});
+
 	}
 }
